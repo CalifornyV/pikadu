549ae81188e52a7027492936e4cb5b8f2b7eb0d5
CalifornyV <83711344+CalifornyV@users.noreply.github.com>
2021-07-02 17:12:06 +0300

    3

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ const editUsername = document.querySelector('.edit-username');
 const editPhotoURL = document.querySelector('.edit-userphoto');
 const userAvatarElem = document.querySelector('.user-avatar');
 
-const postWrapper = document.querySelector('.posts');
+const postsWrapper = document.querySelector('.posts');
 
 
 const toggleAuthDom = () => {
@@ -107,7 +107,7 @@ const setUsers = {
 };
 
 const setPosts = {
-    allPost: [{
+    allPosts: [{
             title: 'Заголовок поста',
             text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный.',
             tags: ['Свежее', 'Лучшее', 'Горячее', 'Подписки'],
@@ -124,15 +124,95 @@ const setPosts = {
             date: '19.11.2020, 20:54:00',
             like: 15,
             comments: 10,
+        },
+        {
+            title: 'Заголовок поста42',
+            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный.',
+            tags: ['Свежее', 'Лучшее', 'Горячее', 'Подписки'],
+            author: '2@mail.ru',
+            date: '19.11.2020, 20:54:00',
+            like: 15,
+            comments: 10,
         }
     ]
 };
 
 const showAllPosts = () => {
+
     let postsHTML = '';
 
-    setPosts.allPosts.forEach((post) => {
-        postsHTML += post
+    setPosts.allPosts.forEach(post => {
+        postsHTML += ` <section class="post">
+                <div class="post-body">
+                    <h2 class="post-title">${post.title}</h2>
+                    <p class="post-text">
+                       ${post.text}
+                    </p>
+                    <p class="post-text">
+                        Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком
+                        агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный. Свой жаренные текстов, вопрос имеет вопроса буквоград алфавит последний великий своего диких lorem скатился, однажды текстами единственное
+                        что парадигматическая ipsum! Повстречался осталось имеет проектах не власти, жаренные злых семантика всеми переписали, взобравшись назад правилами! Встретил свое пустился, ему взгляд прямо домах пояс оксмокс всемогущая по всей
+                        даже собрал то всеми раз все продолжил? Они продолжил подзаголовок текст буквенных оксмокс первую своего до свое по всей дороге силуэт гор что, строчка бросил приставка парадигматическая взобравшись, коварных несколько! Не, проектах,
+                        переписывается составитель языкового, осталось продолжил вдали текстов если что родного выйти по всей страну. Коварный на берегу сбить от всех вдали своего парадигматическая букв прямо реторический дал своих страна рукопись большого,
+                        бросил, которое заглавных живет, рекламных решила. Возвращайся составитель страна над переписали путь себя дал всемогущая жаренные своих инициал курсивных букв lorem диких дорогу заголовок сих на берегу решила точках бросил, взобравшись
+                        выйти текстами коварный. Запятой переписывается текстов она рекламных взобравшись. Lorem она пояс его гор лучше домах, рыбными составитель даль взгляд.
+                    </p>
+
+                    <div class="tags">
+                        <a href="#" class="tag">#свежее</a>
+                        <a href="#" class="tag">#новое</a>
+                        <a href="#" class="tag">#горячее</a>
+                        <a href="#" class="tag">#моё</a>
+                        <a href="#" class="tag">#случайность</a>
+                    </div>
+                </div>
+                <div class="post-footer">
+                    <div class="post-buttons">
+                        <button class="post-button likes">
+                <svg width="19" height="20" class="icon icon-like">
+                    <use xlink:href="img/icons.svg#like"></use>
+                </svg>
+                <span class="likes-counter">
+                    26
+                </span>
+            </button>
+                        <button class="post-button comments">
+                <svg width="21" height="21" class="icon icon-comments">
+                    <use xlink:href="img/icons.svg#comment"></use>
+                </svg>
+                <span class="comments-counter">
+                    157
+                </span>
+            </button>
+                        <button class="post-button save">
+                <svg width="19" height="19" class="icon icon-save">
+                    <use xlink:href="img/icons.svg#save"></use>
+                </svg>
+            </button>
+                        <button class="post-button share">
+                <svg width="17" height="19" class="icon icon-share">
+                    <use xlink:href="img/icons.svg#share"></use>
+                </svg>
+            </button>
+                    </div>
+                    <!-- /.post-buttons -->
+                    <div class="post-autor">
+                        <div class="autor-about">
+                            <a href="" class="autor-username">
+                    artemislamov
+                </a>
+                            <span class="post-time">
+                    5 минут назад
+                </span>
+                        </div>
+                        <!-- /.autor-about -->
+                        <a href="#" class="autor-link"><img src="img/avatar.jpeg" alt="avatar" class="autor-avatar"></a>
+                        <!-- /.autor-link -->
+                    </div>
+                    <!-- /.post-autor -->
+                </div>
+            </section>`;
+
     })
     postsWrapper.innerHTML = postsHTML
 };

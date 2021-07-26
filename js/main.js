f1275327a8cbc2b697424b8a0ae07075692c5ea2
CalifornyV <83711344+CalifornyV@users.noreply.github.com>
2021-07-26 07:57:06 +0300

    8

--- js/main.js
+++ js/main.js
@@ -1,217 +1,256 @@
-//создаем переменную в которую положим кнопку меню
-let menuToggle = document.querySelector('#menu-toggle');
-//создаем переменныую в которую положим меню
-let menu = document.querySelector('.sidebar')
-
-
-const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
-
-const loginElem = document.querySelector('.login');
-const loginFrom = document.querySelector('.login-form');
-const emailInput = document.querySelector('.login-email');
-const passwordInput = document.querySelector('.login-password');
-const loginSignup = document.querySelector('.login-signup');
-
-const userElem = document.querySelector('.user');
-const userNameElem = document.querySelector('.user-name');
-
-const exitElem = document.querySelector('.exit');
-const editElem = document.querySelector('.edit');
-const editContainer = document.querySelector('.edit-container');
-
-const editUsername = document.querySelector('.edit-username');
-const editPhotoURL = document.querySelector('.edit-userphoto');
-const userAvatarElem = document.querySelector('.user-avatar');
-
-const buttonNewPost = document.querySelector('.button-new-post');
-
-const postsWrapper = document.querySelector('.posts');
-
-const addPostElem = document.querySelector('.add-post')
+ const firebaseConfig = {
+     apiKey: "AIzaSyAgWBkm-aPiXXVTsfWjP7Q-NG55FU4-ueU",
+     authDomain: "pikadu-3d2fd.firebaseapp.com",
+     projectId: "pikadu-3d2fd",
+     databaseURL: "https://pikadu-3d2fd-default-rtdb.europe-west1.firebasedatabase.app",
+     storageBucket: "pikadu-3d2fd.appspot.com",
+     messagingSenderId: "523996261363",
+     appId: "1:523996261363:web:3952fb218c9564e1624086",
+     measurementId: "G-MZTEFB3JVY"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
 
 
 
+ firebase.analytics();
 
-const listUsers = [{
-        id: '01',
-        email: '1@mail.ru',
-        password: '12345',
-        displayName: '123JS',
-        photo: 'https://i.ytimg.com/vi/YTopGBfzar0/maxresdefault.jpg'
-    },
-    {
-        id: '02',
-        email: '2@mail.ru',
-        password: '12345',
-        displayName: '321JS'
-    }
-];
 
-const setUsers = {
-    user: null,
-    logIn(email, password, handler) {
-        if (!regExpValidEmail.test(email)) return alert('email not valid')
-        const user = this.getUser(email);
-        if (user && user.password === password) {
-            this.authorizedUser(user);
-            if (handler) {
-                handler();
-            }
+ //создаем переменную в которую положим кнопку меню
+ let menuToggle = document.querySelector('#menu-toggle');
+ //создаем переменныую в которую положим меню
+ let menu = document.querySelector('.sidebar')
 
-        } else {
-            alert('Пользователь с такими данными не найден')
-        }
-    },
-    logOut(handler) {
-        this.user = null;
-        if (handler) {
-            if (handler) {
-                handler();
-            }
 
-        }
+ const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 
-    },
-    signUp(email, password, handler) {
-        if (!regExpValidEmail.test(email)) return alert('email not valid')
-        if (!email.trim() || !password.trim()) {
-            alert('Введите данные')
-            return;
-        }
-        if (!this.getUser(email)) {
-            const user = { email, password, displayName: email.split('@')[0] };
-            listUsers.push(user)
-            this.authorizedUser(user)
-            if (handler) {
-                handler();
-            }
+ const loginElem = document.querySelector('.login');
+ const loginFrom = document.querySelector('.login-form');
+ const emailInput = document.querySelector('.login-email');
+ const passwordInput = document.querySelector('.login-password');
+ const loginSignup = document.querySelector('.login-signup');
 
-        } else {
-            alert('Пользователь с таким email уже зарегистрирован')
-        }
-    },
-    editUser(userName, userPhoto, handler) {
-        if (userName) {
-            this.user.displayName = userName;
-        }
-        if (userPhoto) {
-            this.user.photo = userPhoto;
-        }
-        if (handler) {
-            handler();
-        }
+ const userElem = document.querySelector('.user');
+ const userNameElem = document.querySelector('.user-name');
 
-    },
-    getUser(email) {
-        return listUsers.find((item) => {
-            return item.email === email
-        })
-    },
-    authorizedUser(user) {
-        this.user = user;
-    }
-};
+ const exitElem = document.querySelector('.exit');
+ const editElem = document.querySelector('.edit');
+ const editContainer = document.querySelector('.edit-container');
 
-const setPosts = {
-    allPosts: [{
-            title: 'Заголовок поста',
-            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный.',
-            tags: ['Свежее', 'Лучшее', 'Горячее', 'Подписки'],
-            author: {
-                displayName: 'rir',
-                photo: 'https://i.ytimg.com/vi/qVF4NvPT9UA/maxresdefault.jpg'
-            },
-            date: '11.11.2020, 20:54:00',
-            like: 15,
-            comments: 101,
-        },
-        {
-            title: 'Заголовок поста2',
-            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный.',
-            tags: ['Свежее', 'Лучшее', 'Горячее', 'Подписки'],
-            author: {
-                displayName: 'rick',
-                photo: 'https://i.ytimg.com/vi/YTopGBfzar0/maxresdefault.jpg'
-            },
-            date: '19.11.2020, 20:54:00',
-            like: 415,
-            comments: 10,
-        },
-        {
-            title: 'Заголовок поста42',
-            text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Над моей оксмокс свою живет по всей, запятых маленький не? Рукописи своих силуэт они предложения дал предупредила маленькая осталось запятых букв эта курсивных, языком агентство домах даль своего пор диких вдали гор текстов первую вершину деревни грустный.',
-            tags: ['Свежее', 'Лучшее', 'Горячее', 'Подписки'],
-            author: {
-                displayName: 'rir',
-                photo: 'https://i.ytimg.com/vi/qVF4NvPT9UA/maxresdefault.jpg'
-            },
-            date: '19.11.2020, 20:54:00',
-            like: 15,
-            comments: 12,
-        }
-    ],
-    addPost(title, text, tags, handler) {
+ const editUsername = document.querySelector('.edit-username');
+ const editPhotoURL = document.querySelector('.edit-userphoto');
+ const userAvatarElem = document.querySelector('.user-avatar');
 
-        this.allPosts.unshift({
-            title,
-            text,
-            tags,
-            author,
-            date,
-            like,
-            comments,
+ const buttonNewPost = document.querySelector('.button-new-post');
 
-        })
+ const postsWrapper = document.querySelector('.posts');
 
-        if (handler) {
-            handler();
-        }
-
-    }
-};
-const toggleAuthDom = () => {
-    const user = setUsers.user;
-    console.log('user: ', user);
-
-    if (user) {
-        loginElem.style.display = 'none';
-        userElem.style.display = '';
-        userNameElem.textContent = user.displayName;
-        userAvatarElem.src = user.photo || userAvatarElem.src;
-        buttonNewPost.classList.add('visible');
-    } else {
-        loginElem.style.display = '';
-        userElem.style.display = 'none';
-        buttonNewPost.classList.remove('visible');
-        addPostElem.classList.remove('visible');
-        postsWrapper.classList.add('visible');
-        //
-        addPostElem.classList.add('visible');
-        postsWrapper.classList.remove('visible');
-
-    }
-
-};
-const showAddPost = () => {
-    addPostElem.classList.add('visible');
-    postsWrapper.classList.remove('visible');
-}
-
-const showAllPosts = () => {
-
-        let postsHTML = '';
-
-        setPosts.allPosts.forEach(({ title, text, date, tags, author, like, comments, photo, displayName }) => {
+ const addPostElem = document.querySelector('.add-post')
 
 
 
-                    postsHTML += ` <section class="post">
+
+
+
+ const setUsers = {
+     user: null,
+     initUser(handler) {
+         firebase.auth().onAuthStateChanged(user => {
+             if (user) {
+                 this.user = user;
+             } else {
+                 this.user = null;
+             }
+             if (handler) handler();
+
+         })
+     },
+
+     logIn(email, password, handler) {
+         if (!regExpValidEmail.test(email)) {
+             alert('email not valid');
+             return;
+         }
+         firebase.auth().signInWithEmailAndPassword(email, password).catch(err => {
+             const errCode = err.code;
+             const errMessage = err.message;
+             if (errCode === 'auth/wrong-password') {
+                 console.log(errMessage);
+                 alert('Неверны пароль')
+             } else if (errCode === 'auth/user-not-found') {
+                 console.log(errMessage);
+                 alert('Пользователь не найден')
+             } else {
+
+                 alert(errMessage)
+
+             }
+         });
+         // const user = this.getUser(email);
+         // if (user && user.password === password) {
+         //    this.authorizedUser(user);
+         //    if (handler) {
+         //       handler();
+         //   }
+
+         //  } else {
+         //     alert('Пользователь с такими данными не найден')
+         // }
+     },
+     logOut() {
+
+         firebase.auth().signOut()
+             //this.user = null;
+             // if (handler) {
+             // if (handler) {
+             //     handler();
+
+     },
+     signUp(email, password, handler) {
+         if (!regExpValidEmail.test(email)) return alert('email not valid')
+         if (!email.trim() || !password.trim()) {
+             alert('Введите данные')
+             return;
+         }
+
+         firebase.auth()
+             .createUserWithEmailAndPassword(email, password)
+             .then(data => {
+                 this.editUser(email.substring(0, email.indexOf('@')), null, handler)
+             })
+             .catch(err => {
+                 const errCode = err.code;
+                 const errMessage = err.message;
+                 if (errCode === 'auth/weak-password') {
+                     console.log(errMessage);
+                     alert('Слабый пароль')
+                 } else if (errCode === 'auth/email-already-in-use') {
+                     console.log(errMessage);
+                     alert('email уже используется')
+                 } else {
+
+                     alert(errMessage)
+
+                 }
+             });
+
+         //if (!this.getUser(email)) {
+         //  const user = { email, password, displayName: email.split('@')[0] };
+         //  listUsers.push(user)
+         // this.authorizedUser(user)
+         // if (handler) {
+         //     handler();
+         //  }
+
+         // } else {
+         //     alert('Пользователь с таким email уже зарегистрирован')
+         // }
+     },
+     editUser(displayName, photoURL, handler) {
+
+         const user = firebase.auth().currentUser;
+
+         if (displayName) {
+
+             if (photoURL) {
+                 user.updateProfile({
+                     displayName,
+                     photoURL
+                 })
+             } else {
+                 user.updateProfile({
+                     displayName
+                 }).then(handler);
+             }
+
+         }
+
+
+     },
+     // getUser(email) {
+     //   return listUsers.find((item) => {
+     //         return item.email === email
+     //      })
+     // },
+     // authorizedUser(user) {
+     //     this.user = user;
+     // }
+ };
+
+ const setPosts = {
+     allPosts: [],
+     addPost(title, text, tags, handler) {
+
+         this.allPosts.unshift({
+             id: `postID${(+new Date()).toString(16)}`,
+             title,
+             text,
+             tags: tags.split(',').map(item => item.trim()),
+             author: {
+                 displayName: setUsers.user.displayName,
+                 photo: setUsers.user.photoURL,
+             },
+             date: new Date().toLocaleString(),
+             like: 0,
+             comments: 0,
+
+         })
+
+         firebase.database().ref('post').set(this.allPosts)
+             .then(() => this.getPosts(handler))
+             .then()
+     },
+     getPosts(handler) {
+         firebase.database().ref('post').on('value', snapshot => {
+             this.allPosts = snapshot.val() || [];
+             handler();
+         })
+     }
+ };
+ const toggleAuthDom = () => {
+     const user = setUsers.user;
+
+
+     if (user) {
+         loginElem.style.display = 'none';
+         userElem.style.display = '';
+         userNameElem.textContent = user.displayName;
+         userAvatarElem.src = user.photoURL || userAvatarElem.src;
+         buttonNewPost.classList.add('visible');
+     } else {
+         loginElem.style.display = '';
+         userElem.style.display = 'none';
+         buttonNewPost.classList.remove('visible');
+         addPostElem.classList.remove('visible');
+         postsWrapper.classList.add('visible');
+
+
+     }
+
+ };
+ const showAddPost = () => {
+     addPostElem.classList.add('visible');
+     postsWrapper.classList.remove('visible');
+ }
+
+ const showAllPosts = () => {
+
+
+
+
+         let postsHTML = '';
+
+         setPosts.allPosts.forEach(({ title, text, date, tags, author, like, comments, photo, displayName }) => {
+
+
+
+                     postsHTML += ` <section class="post">
                 <div class="post-body">
                     <h2 class="post-title">${title}</h2>
                     <p class="post-text">${text}</p>
                     
 
-                    <div class="tags">${tags.map(tag => `<a href="#" class="tag">#${tag}</a>`)}</div>
+                    <div class="tags">${tags.map(tag => `<a href="#${tag}" class="tag">#${tag}</a>`)}</div>
                 </div>
                 <div class="post-footer">
                     <div class="post-buttons">
@@ -253,7 +292,10 @@ const showAllPosts = () => {
             </section>`;
 
     })
-    postsWrapper.innerHTML = postsHTML
+    postsWrapper.innerHTML = postsHTML;
+
+     addPostElem.classList.remove('visible');
+        postsWrapper.classList.add('visible');
 };
 
 
@@ -281,7 +323,7 @@ const init = () => {
 
     exitElem.addEventListener('click', event => {
         event.preventDefault();
-        setUsers.logOut(toggleAuthDom);
+        setUsers.logOut();
     });
 
     editElem.addEventListener('click', event => {
@@ -321,12 +363,14 @@ const init = () => {
         }
        setPosts.addPost(title.value, text.value, tags.value, showAllPosts);
 
-       addPostElem.classList.remove('visible')
+       addPostElem.classList.remove('visible');
+       addPostElem.reset();
     });
 
-    showAllPosts();
+setUsers.initUser(toggleAuthDom)
+setPosts.getPosts(showAllPosts)
 
-    toggleAuthDom();
+    
 }
 
 document.addEventListener('DOMContentLoaded', init)
